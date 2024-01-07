ddf557db1caea88b0fec1fab5b771ac88b43bcda
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2024-01-06 21:13:21 -0300

    Add files via upload

---
+++ payload_map.js
@@ -0,0 +1,12 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'etaHEN 1.3B By LM',
+            description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 4.03/4.50/4.51 Only.',
+            fileName: 'etaHEN-1.3B.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.3 beta'
+        },
+
+    ];
